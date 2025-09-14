e8baba82b6583bb696c1ea44fdb7494852be102d
gomescarlosjunior <gomes.carlosjr@gmail.com>
2025-09-13 23:16:23 -0300

    refactor: extract common HostexElementProps type for widget components

--- src/types/hostex-widget.d.ts
+++ src/types/hostex-widget.d.ts
@@ -7,20 +7,18 @@ declare global {
     }
   }
 
+  type HostexElementProps = React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > & {
+    [key: string]: any
+  }
+
   namespace JSX {
     interface IntrinsicElements {
-      'hostex-search-widget': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >
-      'hostex-search-result-widget': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >
-      'hostex-booking-widget': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >
+      'hostex-search-widget': HostexElementProps
+      'hostex-search-result-widget': HostexElementProps
+      'hostex-booking-widget': HostexElementProps
     }
   }
 }
